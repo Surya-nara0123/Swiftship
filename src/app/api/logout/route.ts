ae41293874e54e39c8dda0a2f5f082cbc634c59c
Surya Narayanan <surya23110035@snuchennai.edu.in>
2024-11-02 14:46:11 +0530

    cookie problem fix attempt 1

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -2,8 +2,9 @@ import { NextResponse, NextRequest } from "next/server";
 
 export function POST(request: NextRequest) {
     try {
+        request.cookies.delete('token'); // Ensure matching path for deletion
         const response = NextResponse.next();
-        response.cookies.set('token', '', { expires: new Date(0), path: '/' }); // Ensure matching path for deletion
+        response.cookies.delete('token'); // Ensure matching path for deletion
         return response;
     } catch (error) {
         return NextResponse.json({ message: "Failed to log out" }, { status: 400 });
