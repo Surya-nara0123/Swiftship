294c7ebe3281124765582ef547fcc8adcb833ce1
Surya Narayanan <surya23110035@snuchennai.edu.in>
2024-11-02 13:57:43 +0530

    cookie problem fix attempt

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from "next/server";
 
 export function POST(request: NextRequest) {
     try {
-        const response = NextResponse.json({ message: "Success" }, { status: 200 });
+        const response = NextResponse.next();
         response.cookies.set('token', '', { expires: new Date(0), path: '/' }); // Ensure matching path for deletion
         return response;
     } catch (error) {
