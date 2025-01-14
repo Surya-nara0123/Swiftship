551dc95a032417de01ec03cb44d7f4f20660a5a7
A.none.imus <surya.nara0123@gmail.com>
2025-01-14 18:27:11 +0530

    hmmm?

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,19 +1,35 @@
 import { NextResponse, NextRequest } from "next/server";
 
 export function PUT(request: NextRequest) {
-    try {
-        // request.cookies.delete('token'); // Ensure matching path for deletion
-        const response = NextResponse.json({ message: "Logged out" }, { status: 200 });
-        console.log("Request cookies before deleting cookie", request.cookies.getAll());
-        // console.log(response.cookies.getAll(), request.cookies.getAll());
-        for(const cookie of request.cookies.getAll()) {
-            response.cookies.set(cookie.name, cookie.value);
-        }
-        response.cookies.delete('token'); // Ensure matching path for deletion
-        // console.log(response.cookies.getAll()), request.cookies.getAll();
-        console.log("Response cookies after deleting cookie", response.cookies.getAll());
-        return response;
-    } catch (error) {
-        return NextResponse.json({ message: "Failed to log out" }, { status: 400 });
+  try {
+    const isLocalhost = request.url.includes("localhost");
+    // request.cookies.delete('token'); // Ensure matching path for deletion
+    const response = NextResponse.json(
+      { message: "Logged out" },
+      { status: 200 }
+    );
+    console.log(
+      "Request cookies before deleting cookie",
+      request.cookies.getAll()
+    );
+    // console.log(response.cookies.getAll(), request.cookies.getAll());
+    for (const cookie of request.cookies.getAll()) {
+      response.cookies.set(cookie.name, cookie.value);
     }
+    response.cookies.set("token", "", {
+      maxAge: 0,
+      path: "/",
+      secure: true,
+      sameSite: "lax",
+      ...(isLocalhost ? {} : { domain: "swiftship-nine.vercel.app" }),
+    });
+    // console.log(response.cookies.getAll()), request.cookies.getAll();
+    console.log(
+      "Response cookies after deleting cookie",
+      response.cookies.getAll()
+    );
+    return response;
+  } catch (error) {
+    return NextResponse.json({ message: "Failed to log out" }, { status: 400 });
+  }
 }
