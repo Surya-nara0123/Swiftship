3d4b5d609491d54787aca2780a204ce9aea6b569
A.none.imus <surya.nara0123@gmail.com>
2025-01-14 18:24:13 +0530

    ig one way to solve it

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -6,6 +6,9 @@ export function PUT(request: NextRequest) {
         const response = NextResponse.json({ message: "Logged out" }, { status: 200 });
         console.log("Request cookies before deleting cookie", request.cookies.getAll());
         // console.log(response.cookies.getAll(), request.cookies.getAll());
+        for(const cookie of request.cookies.getAll()) {
+            response.cookies.set(cookie.name, cookie.value);
+        }
         response.cookies.delete('token'); // Ensure matching path for deletion
         // console.log(response.cookies.getAll()), request.cookies.getAll();
         console.log("Response cookies after deleting cookie", response.cookies.getAll());
